908b20ec0a60d7c886b56f690a5ca067a4df7b2c
Hizkiel-Mesfin <mesfinezekiel@gmail.com>
2022-06-30 13:34:40 +0200

    added a js file

---
+++ solution.js
@@ -0,0 +1,8 @@
+console.log("Hello World");
+
+console.log("This is my first js file");
+
+/**
+ * #1 The statment does not print when i remove the parenthesis.
+ * #2 There is an error and it's a syntax error. It says unexpected   string
+ */
